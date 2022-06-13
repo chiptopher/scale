d20c8ffde9601d05f9807d9fd288f0b5dce00f80
chiptopher <chrisboyerdev@gmail.com>
2022-06-13 11:10:12 -0400

    Better styles and a cooler footer

--- src/App.tsx
+++ src/App.tsx
@@ -22,53 +22,79 @@ function App() {
 
     return (
         <Container>
-            <HoverContainer>
-                <Input
-                    label="Width"
-                    onChange={e => setFromWidth(parseInt(e.target.value))}
-                    value={fromWidth || ''}
-                />
-                <Input
-                    label="Height"
-                    onChange={e => setFromHeight(parseInt(e.target.value))}
-                    value={fromHeight || ''}
-                />
-            </HoverContainer>
-            <p className="scales-to">Scales To</p>
-            <HoverContainer>
-                <Input
-                    label="Width"
-                    onChange={e => {
-                        const newWidth = parseInt(e.target.value);
-                        setToWidth(newWidth);
-                        if (fromWidth && fromHeight) {
-                            const divisor = newWidth / fromWidth;
-                            setDivisor(divisor);
-                            setToHeight(fromHeight * divisor);
-                        }
-                    }}
-                    onFocus={clear}
-                    value={toWidth || ''}
-                />
-                <Input
-                    label="Height"
-                    onChange={e => {
-                        const newHeight = parseInt(e.target.value);
-                        setToHeight(newHeight);
-                        if (fromWidth && fromHeight) {
-                            const divisor = newHeight / fromHeight;
-                            setDivisor(divisor);
-                            setToWidth(fromWidth * divisor);
-                        }
-                    }}
-                    onFocus={clear}
-                    value={toHeight || ''}
-                />
-            </HoverContainer>
-            <p>
-                <span>Scale</span>&nbsp;
-                {divisor}
-            </p>
+            <main>
+                <HoverContainer>
+                    <Input
+                        label="Width"
+                        onChange={e => setFromWidth(parseInt(e.target.value))}
+                        value={fromWidth || ''}
+                    />
+                    <Input
+                        label="Height"
+                        onChange={e => setFromHeight(parseInt(e.target.value))}
+                        value={fromHeight || ''}
+                    />
+                </HoverContainer>
+                <p className="scales-to">Scales To</p>
+                <HoverContainer>
+                    <Input
+                        label="Width"
+                        onChange={e => {
+                            const newWidth = parseInt(e.target.value);
+                            setToWidth(newWidth);
+                            if (fromWidth && fromHeight) {
+                                const divisor = newWidth / fromWidth;
+                                setDivisor(divisor);
+                                setToHeight(fromHeight * divisor);
+                            }
+                        }}
+                        onFocus={clear}
+                        value={toWidth || ''}
+                    />
+                    <Input
+                        label="Height"
+                        onChange={e => {
+                            const newHeight = parseInt(e.target.value);
+                            setToHeight(newHeight);
+                            if (fromWidth && fromHeight) {
+                                const divisor = newHeight / fromHeight;
+                                setDivisor(divisor);
+                                setToWidth(fromWidth * divisor);
+                            }
+                        }}
+                        onFocus={clear}
+                        value={toHeight || ''}
+                    />
+                </HoverContainer>
+                <p>
+                    <span>Scale</span>&nbsp;
+                    {divisor}
+                </p>
+            </main>
+            <footer>
+                <ul>
+                    <li>
+                        <a
+                            href="https://github.com/chiptopher/scale"
+                            rel="noreferrer"
+                            target="_blank"
+                        >
+                            Source
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://chrismboyer.com"
+                            rel="noreferrer"
+                            target="_blank"
+                        >
+                            &copy; {new Date().getFullYear()} Christopher M.
+                            Boyer
+                        </a>
+                        <span className="face">ಠ_ಠ</span>
+                    </li>
+                </ul>
+            </footer>
         </Container>
     );
 }
@@ -81,10 +107,30 @@ const Container = styled.div`
     flex-direction: column;
     width: 100vw;
     height: 100vh;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
 
     p {
         font-size: 2rem;
+    }
+
+    main {
+        padding-top: 2rem;
+    }
+
+    footer {
+        font-size: 1rem;
+        text-align: center;
+
+        ul {
+            list-style: none;
+
+            li {
+                margin-top: 1rem;
+            }
+        }
+        .face {
+            margin-left: 2rem;
+        }
     }
 `;
